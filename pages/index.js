bf0fe64ed41699622e7db2d0dfb151a63fe3dd12
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-07-16 21:35:23 +0600

    dark mode test

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
+import useTheme from "../hooks/useTheme"
 
 export default function Home() {
+  const { toggleTheme } = useTheme()
   return (
     <div>
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
+      <button className="px-5 py-2 rounded-lg border-2 bg-blue-600 text-white font-bold" onClick={toggleTheme}>Change theme</button>
     </div>
   )
 }
